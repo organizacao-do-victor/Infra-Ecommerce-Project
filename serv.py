198d16e7263bc047d1c10cad1aeebbdc8e883f46
RebelWyvern <carolina.fracao@acad.pucrs.br>
2022-12-15 12:14:16 -0300

    ansible

--- serv.py
+++ serv.py
@@ -9,7 +9,7 @@ class html(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers ()
         message = '<!DOCTYPE html><html><head><title>AAAAAAAAAAAAAAAAAAAAAAAAA</title><meta charset="UTF-8"></head><body>'
-        message += '<h1 style="font-family:arial black; font-size:360px;">NADA</h1>'
+        message += 
         message += '</body></html>'
         self.wfile.write (bytes(message, "utf8"))
 
