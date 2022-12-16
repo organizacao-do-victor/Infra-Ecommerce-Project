afa8771629d48c651de1a8595f463e563b0a30c3
Raul Rodrigues <raulferrodrigues@gmail.com>
2022-12-16 14:13:49 -0300

    mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,33 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const mongoose = require('mongoose')
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
 
-app.listen(9999);
+function mongo() {
+  const mongodbUrl = 'mongodb://root:pass@3.87.237.32:27017/ecommerce?authMechanism=DEFAULT&authSource=admin&tls=false'
+  mongoose.set('strictQuery', false)
+  mongoose.connect(mongodbUrl)
+}
+mongo()
+
+const userSchema = {
+  user: String,
+  pwd: String
+}
+
+const User = mongoose.model("User", userSchema)
+
+User.find(function(err, users) {
+  if (!err) { 
+    console.log(users)
+  } else {
+    console.log(err)
+  }
+})
+
+const app = express()
+app.use(express.static('public'))
+app.get('/', function(req, res) {
+   res.send("Hello world!")
+})
+
+app.listen(9999)
