302ddbce8ab2bb9497d10c21c4fdf36afa4e1c17
RukNdf <48357253+RukNdf@users.noreply.github.com>
2022-12-19 11:00:25 -0300

    Delete automation directory

--- automation/format.py
+++
@@ -1,9 +0,0 @@
-dic = {}
-
-with open('output') as file:
-	for line in file:
-		line = line.split(' = ')
-		dic[line[0]] = line[1].strip()
-
-with open('hosts','w+') as file:
-	file.write('[vm]\n {} ansible_user=ubuntu'.format(dic['ec2_IP']))
