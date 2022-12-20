f3fe49cb2b37013161dec594ec696ba814c74bfe
rukndf <a@a.com>
2022-12-20 11:08:08 -0300

    ansible

--- automation/format.py
+++ automation/format.py
@@ -5,17 +5,17 @@ with open('output') as file:
 		line = line.split(' = ')
 		dic[line[0]] = line[1].replace('"','').strip()
 
-with open('ansible-local/hosts','w+') as file:
+with open('ansible/hosts','w+') as file:
 	file.write('[vm]\n')
 	file.write('{} ansible_user=ubuntu'.format(dic['Ansible-Public-IP']))
 
-with open('ansible-remoto/hosts', 'w+') as file:
+with open('ansible/remote-files/hosts', 'w+') as file:
 	file.write('[front]\n{} ansible_user=ubuntu\n'.format(dic['Front-App-Public-IP']))
 	file.write('[grafana]\n{} ansible_user=ubuntu\n'.format(dic['Grafana-Public-IP']))
 	file.write('[db]\n{} ansible_user=ubuntu\n'.format(dic['Databases-Public-IP']))
 
-with open('ansible-remoto/s3', 'w+') as file:
+with open('ansible/remote-files/s3', 'w+') as file:
 	file.write(dic['Bucket-name'])
 
-with open('ansible-remoto/dotenv', 'w+') as file:
+with open('ansible/remote-files/dotenv', 'w+') as file:
 	file.write('bucketname={}'.format(dic['Bucket-name']))
