27cf1d540ea41f4a4ad6ff519dc122eb6effabe1
rukndf <a@a.com>
2023-01-02 12:55:06 -0300

    dotenv

--- automation/format.py
+++ automation/format.py
@@ -5,6 +5,12 @@ with open('output') as file:
 		line = line.split(' = ')
 		dic[line[0]] = line[1].replace('"','').strip()
 
+with open('../banco/dbcred') as file:
+	for line in file:
+		line = line.split(' = ')
+		dic[line[0]] = line[1].replace('"','').strip()
+
+
 with open('ansible/hosts','w+') as file:
 	file.write('[vm]\n')
 	file.write('{} ansible_user=ubuntu'.format(dic['Ansible-Public-IP']))
@@ -18,4 +24,13 @@ with open('ansible/remote-files/s3', 'w+') as file:
 	file.write(dic['Bucket-name'])
 
 with open('ansible/remote-files/dotenv', 'w+') as file:
-	file.write('bucketname={}'.format(dic['Bucket-name']))
+	file.write('MONGOUSER={}\n'.format(dic['mongoUser']))
+	file.write('MONGOPASSWORD={}\n'.format(dic['mongoPass']))
+	file.write('MONGOHOST={}\n'.format(dic['Databases-Public-IP']))
+	file.write('MONGODATABASE={}\n'.format(dic['mongoDB']))
+	file.write('BUCKETNAME={}\n'.format(dic['Bucket-name']))
+	file.write('PGHOST={}\n'.format(dic['Databases-Public-IP']))
+	file.write('PGUSER={}\n'.format('postgres'))
+	file.write('PGDATABASE={}\n'.format(dic['pgreDB']))
+	file.write('PGPASSWORD={}\n'.format(dic['pgrePass']))
+	file.write('PGPORT={}\n'.format('5432'))
