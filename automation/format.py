794b668c4e252dd0e0639595c1c7f66920b62ae3
RukNdf <48357253+RukNdf@users.noreply.github.com>
2023-01-03 11:32:39 -0300

    automate db user pass

--- automation/format.py
+++ automation/format.py
@@ -1,13 +1,11 @@
 dic = {}
 dic['pgreUsr']='postgres'
 dic['pgrePort']='5432'
+dic['pgreDB']='Produtos'
+dic['mongoDB']='backend-db'
+NUMC = 32
 
 with open('output') as file:
-	for line in file:
-		line = line.split(' = ')
-		dic[line[0]] = line[1].replace('"','').strip()
-
-with open('../banco/.env') as file:
 	for line in file:
 		line = line.split(' = ')
 		dic[line[0]] = line[1].replace('"','').strip()
@@ -41,3 +39,18 @@ with open('ansible/remote-files/ansible-vars.yaml', 'w+') as file:
 	file.write('pgreHost: {}\n'.format(dic['Databases-Public-IP']))
 	file.write('pgreUser: {}\n'.format(dic['pgreUsr']))
 	file.write('pgrePass: {}\n'.format(dic['pgrePass']))
+
+
+import string
+import secrets
+def generateRandom():
+	c = string.ascii_letters+string.digits
+	return ''.join(secrets.choice(c) for i in range(NUMC))
+
+with open('../banco/.env', 'w+') as file:
+	file.write('pgreUser: {}\n'.format(generateRandom()))
+	file.write('pgrePass: {}\n'.format(generateRandom()))
+	file.write('pgreDB: {}\n'.format(dic['pgreDB']))
+	file.write('mongoUser: {}\n'.format(generateRandom()))
+	file.write('mongoPass: {}\n'.format(generateRandom()))
+	file.write('mongoDB: {}\n'.format(dic['mongoDB']))
