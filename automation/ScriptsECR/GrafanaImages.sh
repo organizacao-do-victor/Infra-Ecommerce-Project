187b28270fe0d051754489ddf0259e77258daf66
RebelWyvern <carolina.fracao@acad.pucrs.br>
2023-01-04 09:10:11 -0300

    scripts para colocar as imagens no repositorio do ecr, ainda falta colocar o nome de algumas imagens restantes

--- automation/ScriptsECR/GrafanaImages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ACCOUNT_ID="353681168015"
-AWS_DEFAULT_REGION="us-east-1"
-REPOSITORY_URI="$ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
-IMAGE_REPO_NAME="monitoring"
-docker pull "imagens necessárias para monitoramento":latest
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI 
-docker tag {}:latest $REPOSITORY_URI/$IMAGE_REPO_NAME:{}
-docker push $REPOSITORY_URI/$IMAGE_REPO_NAME:
